21ad3d077c0295291e99e2c34e888c2225324303
lgridsaffan <shaffanabrorr6s@gmail.com>
2020-09-11 22:10:12 +0700

    initial commit

---
+++ latihan2.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  // membuat variabel
+  String nama = 'Shaffan';
+  int umur = 19;
+  bool isJomblo = true;
+
+  var alamat = 'Prob, Jatim';
+
+  // mencetak variabel
+  print("Nama saya $nama. Umur $umur tahun.");
+  print("Menikah: $isJomblo");
+  print("Alamat: $alamat");
+}
---
+++ latihan8.dart
@@ -0,0 +1,6 @@
+main(List<String> args) {
+  var hello = "Rahasia";
+  for (var i = 0; i < hello.length; i++) {
+    print(hello[i]);
+  }
+}
