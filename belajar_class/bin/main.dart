e0383f554ab39c35dbf43003b951489e20684a16
lgridsaffan <shaffanabrorr6s@gmail.com>
2020-10-07 19:19:24 +0700

    latihan class

--- belajar_class/bin/main.dart
+++ belajar_class/bin/main.dart
@@ -1,12 +1,24 @@
 import 'dart:io';
 
-luas_segiempat({p = 1, l = 0}) {
-  var hasil = p * l;
-  return hasil;
+main(List<String> args) {
+  PersegiPanjang kotak1, kotak2;
+
+  kotak1 =
+      new PersegiPanjang(); // keyword new optional, tetapi bhs dart menyarankan tidak perlu ada new
+  kotak1.panjang = 5;
+  kotak1.lebar = 2;
+
+  kotak2 = PersegiPanjang();
+  kotak2.panjang = double.tryParse(stdin.readLineSync());
+  kotak2.lebar = double.tryParse(stdin.readLineSync());
+  print(kotak2.hitungLuas());
 }
 
-main(List<String> args) {
-  // var p = double.tryParse(stdin.readLineSync());
-  // var l = double.tryParse(stdin.readLineSync());
-  print(luas_segiempat(2, 3));
+class PersegiPanjang {
+  double panjang, lebar;
+
+  double hitungLuas() {
+    return this.panjang *
+        this.lebar; // this itu optional, bisa ditulis bisa tidak :D
+  }
 }
